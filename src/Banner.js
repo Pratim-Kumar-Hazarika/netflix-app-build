a5dca245ea96b1be135af7341ed95e987cde8916
PratimKumarHazarika <PratimKumarHazarika@users.noreply.github.com>
2021-02-09 17:09:16 +0530

    commit : truncate

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Banner.css'
 
 export default function Banner() {
+    function truncate (string,n){
+        return string?.length > n ? string.substr(0,n-1) +"..." : string;
+    }
     return (
       <header 
       className ="banner"
@@ -16,7 +19,7 @@ export default function Banner() {
             <div className="banner_buttons">
                 <button className="banner_button">Play</button>
                 <button className="banner_button">My List</button>
-                <div className="banner_description">Inspired by the adventures of Arsène Lupin, gentleman thief Assane Diop sets out to avenge his father for an injustice inflicted by a wealthy family.</div>
+                <div className="banner_description">{truncate("Inspired by the adventures of Arsène Lupin, gentleman thief Assane Diop sets out to avenge his father for an injustice inflicted by a wealthy familyhttps://tipsmake.com/data/images/top-black-cover-photos-for-those-who-are-sad-picture-13-j3KHOP9sE.jpg.",150)}</div>
             </div>
         </div>
             
