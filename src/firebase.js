f0540ac06527e323360e7399e2eb0d79162c1713
PratimKumarHazarika <PratimKumarHazarika@users.noreply.github.com>
2021-02-09 15:58:38 +0530

    commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDgrgMqgVH7HLq5IXmaJfFDVCqTQ0v6lb4",
+    authDomain: "netflix-app-build.firebaseapp.com",
+    projectId: "netflix-app-build",
+    storageBucket: "netflix-app-build.appspot.com",
+    messagingSenderId: "349052805390",
+    appId: "1:349052805390:web:47a7673eccada6abc7f75f"
+  };
